3d30f5bd504e60c7589ba95d854d67e2571f6b4e
Dr. Abder-Rahman Ali <abder@cs.stir.ac.uk>
2021-02-27 17:01:02 -0400

    Add files via upload

---
+++ perceptron_rosenblatt_AND.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+input_size = 2 # number of features
+lr = 0.1 
+epochs = 10
+
+X = np.array([
+        	  [0,0],
+              [0,1],
+              [1,0],
+              [1,1]
+             ])
+
+W = np.array([0,0,0]) # initialize weights
+
+y = np.array([
+			   0,
+			   0,
+			   0,
+			   1
+			  ])
+
+def activation_function(z):
+	if z >= 0:
+		return 1
+	else:
+		return 0
+
+for epoch in range(epochs):
+	n = 0 # number of correct classifications counter
+	print('EPOCH# ' + str(epoch))
+	for i in range(y.shape[0]):
+		x = np.insert(X[i],0,1)
+		z = np.dot(W,x)
+		a = activation_function(z)
+		error = y[i] - a
+		if error == 0:
+			n = n + 1
+		delta_w = lr * error * x
+
+		if y[i] != a:
+			W = W + delta_w # update the weight vector
+		else:
+			W = W
+
+	if n == 4:
+		print('Congratulations! The perceptron converged successfully.')
+		print('Those are the weights:')
+		print(W)
+		break
